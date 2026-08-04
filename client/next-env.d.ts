9731c8ef6b9c34c6f182403c84c5f4fd3fc13ddf
rambharvad012-art <rambharvad012@gmail.com>
2026-08-04 20:03:08 +0530

    feat(client): add Next.js environment types

---
+++ client/next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE:
+// This file should not be edited manually.
+// It is used by Next.js to provide TypeScript support.
