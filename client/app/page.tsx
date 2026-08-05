0ef23093b90ee90ea07d0baa394eab2955947f3b
rambharvad012-art <rambharvad012@gmail.com>
2026-08-05 13:26:54 +0530

    Import FeatureCards component in HomePage

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import FeatureCards from "./components/FeatureCards";
 
 export default function HomePage() {
   return (
