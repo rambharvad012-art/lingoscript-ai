3c635342c4abf1ce5e9fb76cf1fe42dd6d5ff596
rambharvad012-art <rambharvad012@gmail.com>
2026-08-09 00:43:06 +0530

    Update import paths and wrap content in main tag

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,15 +1,15 @@
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import FeatureCards from "../components/FeatureCards";
-import UploadSection from "../components/UploadSection";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import FeatureCards from "./components/FeatureCards";
+import UploadSection from "./components/UploadSection";
 
 export default function HomePage() {
   return (
-    <>
+    <main className="min-h-screen bg-black text-white">
       <Navbar />
       <Hero />
       <FeatureCards />
       <UploadSection />
-    </>
+    </main>
   );
 }
